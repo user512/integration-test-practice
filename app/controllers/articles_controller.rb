613f7df8669f0b035115cc5fe27952e629210b77
Tom Lee <tlee418@g.ucla.edu>
2015-11-21 23:55:24 -0800

    Restrict access with controler actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,26 +22,31 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    set_article
+    if @article.user != current_user
+      flash[:danger] = "You can only edit your own article."
+      redirect_to root_path
+    end
   end
 
   def update
-    set_article
-    if @article.update(article_params)
-      flash[:success] = "Article has been updated"
-      redirect_to @article
+    if @article.user != current_user
+      flash[:danger] = "You can only edit your own article."
+      redirect_to root_path
     else
-      flash.now[:danger] = "Article has not been updated"
-      render :edit
+      if @article.update(article_params)
+        flash[:success] = "Article has been updated"
+        redirect_to @article
+      else
+        flash.now[:danger] = "Article has not been updated"
+        render :edit
+      end
     end
   end
 
   def show
-    set_article
   end
 
   def destroy
-    set_article
     if @article.destroy
       flash[:success] = "Article has been deleted"
       redirect_to articles_path
